28d414dac036651cef8bb8136f3b2958b047acdf
pedro fernandes <thephfernandes@gmail.com>
2020-10-13 13:11:03 +0200

    quick setup and populate with template data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="team-list">
+      <div class="team-item" v-for="team in teams" :key="team.id">
+        {{ team.name }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      teams: [{ id: 0, name: 'team a' }, { id: 1, name: 'team b' }, { id: 2, name: 'team c' }]
+    }
   }
 })
 </script>
